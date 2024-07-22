8e48191f9a01c3c2414ca49b2307f9e1e78c6e5c
Lexiana <schaller.lexi@gmail.com>
2024-07-22 11:38:27 +0200

    restart project in tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
-  Links,
+  Links, LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -7,24 +8,30 @@ import {
 } from "@remix-run/react";
 import "./tailwind.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+
+export const meta: MetaFunction = () =>{
+  return [
+    {charset: "utf-8"},
+    {title: "Calculator"},
+    {viewport: "width=device-width,initial-scale=1"}
+  ];
+};
+
+
+export default function App() {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
 }
 
-export default function App() {
-  return <Outlet />;
-}
